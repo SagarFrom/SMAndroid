d120b089f3977f0c9a9f9cf2cce1109dc0dae9c2
SagarFrom <Juanxps@MacBook-Pro-de-Pedro.local>
2016-05-04 01:33:55 -0500

    creation_of_proyect
    The creation of project

---
+++ Savemymoney/app/src/main/java/pe/edu/upc/savemymoney/MainActivitySMMoney.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.savemymoney;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivitySMMoney extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity_smmoney);
+    }
+}
